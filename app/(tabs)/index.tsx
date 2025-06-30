8ffe81e55fc393caaea89528dd8869ca328792d7
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-01 00:53:20 +0530

    Commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,23 @@
 import { Link } from "expo-router";
-import { Image, Pressable, Text, TouchableOpacity, View } from "react-native";
+import { View } from "react-native";
 import { styles } from "../../styles/auth.styles.js";
 
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello</Text>
+
+      <Link href={"/notifications"}>Feed Screen in Tabs</Link>
+
+
+
+      {/* <Text style={styles.title}>Hello</Text> */}
 
       {/* <Image 
       source={require("../assets/images/icon.png")}
       style={{width:100, height:100}}/> */}
 
-      <Image 
+      {/* <Image 
       source={{uri: "https://images.unsplash.com/photo-1748392029321-58793571f850?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"}}
       style={{
         width: 200,
@@ -21,15 +26,15 @@ export default function Index() {
       }}/>
 
       <Link href={"/notifications"}> Visit Notifications Screen</Link>
-      
+       */}
     
-      <TouchableOpacity onPress={() => alert("You touched")}>
+      {/* <TouchableOpacity onPress={() => alert("You touched")}>
         <Text>Touch Me</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
 
-      <Pressable onPress={()=> alert("You pressed me")}>
+      {/* <Pressable onPress={()=> alert("You pressed me")}>
         <Text> Press Me</Text>
-      </Pressable>
+      </Pressable> */}
     </View>
   );
 }
