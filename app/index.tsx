79e7806385e8ba9ab14f56bf58b480ee8a739604
Jatin5760 <jatinsharma14202003@gmail.com>
2025-06-30 23:48:47 +0530

    Commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,35 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import { styles } from "../styles/auth.styles.js";
+
 
 export default function Index() {
   return (
-    <View
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello</Text>
+
+      {/* <Image 
+      source={require("../assets/images/icon.png")}
+      style={{width:100, height:100}}/>
+
+      <Image 
+      source={{uri: "https://images.unsplash.com/photo-1748392029321-58793571f850?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"}}
       style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+        width: 200,
+        height: 200,
+        resizeMode: "cover"
+      }}/> */}
+
+      <Link href={"/notifications"}> Visit Notifications Screen</Link>
+      
+      {/* <TouchableOpacity onPress={() => alert("You touched")}>
+        <Text>Touch Me</Text>
+      </TouchableOpacity>
+
+      <Pressable onPress={()=> alert("You pressed me")}>
+        <Text> Press Me</Text>
+      </Pressable> */}
     </View>
   );
 }
+
