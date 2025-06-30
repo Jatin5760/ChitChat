a090599e6e38d845cf30e03b24e4640c8ebe6177
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-01 00:39:33 +0530

    Commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,36 @@
+import { Link } from "expo-router";
+import { Image, Pressable, Text, TouchableOpacity, View } from "react-native";
+import { styles } from "../../styles/auth.styles.js";
+
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello</Text>
+
+      {/* <Image 
+      source={require("../assets/images/icon.png")}
+      style={{width:100, height:100}}/> */}
+
+      <Image 
+      source={{uri: "https://images.unsplash.com/photo-1748392029321-58793571f850?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"}}
+      style={{
+        width: 200,
+        height: 200,
+        resizeMode: "cover"
+      }}/>
+
+      <Link href={"/notifications"}> Visit Notifications Screen</Link>
+      
+    
+      <TouchableOpacity onPress={() => alert("You touched")}>
+        <Text>Touch Me</Text>
+      </TouchableOpacity>
+
+      <Pressable onPress={()=> alert("You pressed me")}>
+        <Text> Press Me</Text>
+      </Pressable>
+    </View>
+  );
+}
+
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{flex: 1}}>
+        <Stack screenOptions={{headerShown: false}}>
+          {/* To remove the header */}
+          {/* <Stack.Screen name = "index" options={{ headerShown:false}}/> */}
+          {/* <Stack.Screen name="index" options={{ title: "Feed" }} />
+          <Stack.Screen
+            name="notifications"
+            options={{ title: "Notifications", headerShown: false }}
+          /> */}
+        </Stack>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
