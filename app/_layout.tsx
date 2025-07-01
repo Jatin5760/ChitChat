83861f92b5bc781349ce3ec246d3c7ece8a6fb18
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-01 18:28:40 +0530

    Commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,16 @@
-import InitialLayout from '@/components/InitialLayout';
-import { ClerkProvider } from '@clerk/clerk-expo';
-import { tokenCache } from '@clerk/clerk-expo/token-cache';
+import InitialLayout from "@/components/InitialLayout";
+import ClearkAndConvexProvider from "@/providers/ClearkAndConvexProvider";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
-    <SafeAreaProvider>
-      <SafeAreaView style={{flex: 1, backgroundColor: "black"}}>
-        <InitialLayout/>
-        {/* <Stack screenOptions={{headerShown: false}}> */}
+    <ClearkAndConvexProvider>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+          <InitialLayout />
+          {/* <Stack screenOptions={{headerShown: false}}> */}
           {/* To remove the header */}
           {/* <Stack.Screen name = "index" options={{ headerShown:false}}/> */}
           {/* <Stack.Screen name="index" options={{ title: "Feed" }} />
@@ -20,9 +18,9 @@ export default function RootLayout() {
             name="notifications"
             options={{ title: "Notifications", headerShown: false }}
           /> */}
-        {/* </Stack> */}
-      </SafeAreaView>
-    </SafeAreaProvider>
-    </ClerkProvider>
+          {/* </Stack> */}
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ClearkAndConvexProvider>
   );
 }
